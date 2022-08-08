ba6e79209763065dd8802a2d5637856a3e143169
Zuzee00 <2muhammadazhar@gmail.com>
2022-08-09 04:59:17 +0500

    💰 UK Gender Pay gap

--- 2022/Week26/week26.R
+++ 2022/Week26/week26.R
@@ -28,33 +28,36 @@ df <- paygap |> filter(substr(due_date,1,4)=="2022")
 df |> ggplot( aes(diff_mean_hourly_percent, fct_rev(employer_size),
                          color=employer_size))+
      ggridges::stat_density_ridges(
-       aes(fill = factor(stat(quantile))),
+       aes(fill = stat(quantile)),
        geom = "density_ridges_gradient",
        quantile_lines = TRUE, quantiles = 4, 
-       color = "black", alpha = .8, size = 1
+       color = "black", alpha = .8, size = 0.8
      ) +
   scale_fill_met_d(name = "Morgenstern", direction = 1)+
-  scale_x_continuous(expand = c(0,0)) + 
-  scale_y_discrete( expand = c(0,0),
-                       limits=c('Not Provided',
-                                'Less than 250', '250 to 499',
-                                '500 to 999', '1000 to 4999',
-                                '5000 to 19,999','20,000 or more'))+
-    labs(
+  scale_x_continuous(expand = c(0,-1), limits = c(-105, 105)) + 
+  scale_y_discrete( expand = c(0,0), limits=c('Not Provided',
+                                              'Less than 250', '250 to 499',
+                                              '500 to 999', '1000 to 4999',
+                                              '5000 to 19,999',
+                                              '20,000 or more'))+
+  labs(
     title = "UK Gender Pay Gap",                
     subtitle = "Mean % difference between male and female hourly pay.
 (negative = women's mean hourly pay is higher) ",
     caption = "Muhammad Azhar | #TidyTuesday Week 26 | Data: ONS.gov.uk",
-    x = "Mean % difference hourly pay") +
+    x = "Mean % difference hourly pay",
+    y = "Compnay Size --->") +
   
   coord_cartesian(clip="off") +
   theme(
     panel.grid = element_blank(),
     axis.title.x = element_text (size=11, color="grey20",
                                      margin = margin(10,0,0,0) ),
-    axis.title.y = element_blank(),
+    axis.title.y = element_text (size=11, color="grey20", hjust=0.4,
+                                 margin = margin(r=5)),
     axis.text.x = element_text(size=11, color="black"),
     axis.text.y = element_text(size=11, color="black"),
+    
     plot.title = element_text(size=24, face="bold",
                               margin=margin(10,0,10,0)),
     plot.title.position = 'plot',
@@ -64,7 +67,10 @@ df |> ggplot( aes(diff_mean_hourly_percent, fct_rev(employer_size),
                                 margin=margin(20,0,0,0)),
     plot.background = element_rect(color = bg, fill=bg),
     plot.margin = margin(30,30,30,30),
-    legend.position="none"
+    legend.title = element_text("Quantiles"),
+    legend.text = element_text(size=12),
+    legend.position = "top",
+    legend.justification="right"
   )
 
 # Save plot ---------------------------------------------------------- 

187356bfb6765c0b2e518ac2026cb368ab8e4b8d
imagineazhar <azhar116406262@gmail.com>
2022-08-25 15:36:00 +0500

    💾 week 34 final

--- 2022/Week_34/week_34.R
+++ 2022/Week_34/week_34.R
@@ -1,6 +1,5 @@
 library(tidytuesdayR)
 library(tidyverse)
-library(showtext)
 library(MetBrewer)
 library(patchwork)
 
@@ -22,13 +21,13 @@ raw_chips <- read.csv("chip_dataset.csv")
 chips <- raw_chips|>
   janitor::clean_names()|>
   mutate(year = lubridate::year(as.Date(release_date)))|>
-  select(product, year, type, process_size_nm, tdp_w, die_size_mm_2,
-         transistors_million, freq_m_hz, vendor)|>
+  select(product, year, type, die_size_mm_2, transistors_million)|>
   drop_na()
 
 
 
 # plot----------------------------------------------------------
+
 
 p1 <- chips |> ggplot(aes(x=year, y=transistors_million, color=type))+
   geom_point(size = 3.5, alpha=0.25)+
@@ -36,7 +35,7 @@ p1 <- chips |> ggplot(aes(x=year, y=transistors_million, color=type))+
   scale_y_log10()+
   geom_smooth(method = 'loess', se=FALSE)+
   labs(
-    title = "The number of transistors on a chip is still increasing exponentially over time, especially for GPUs.",
+    title = "Transistor Count.",
     x = "Year",
     y = "Tansistors (millions)") +
   coord_cartesian(clip="off") +
@@ -51,18 +50,18 @@ p1 <- chips |> ggplot(aes(x=year, y=transistors_million, color=type))+
         plot.background = element_rect(color = bg, fill=bg),
         plot.margin = margin(20,30,0,30),
         plot.title.position = "plot",
-        legend.position = "top",
+        legend.position = "bottom",
         legend.title = element_blank())
 
 # plot 2 ------------------------------------------------------
 
 p2 <- chips |> ggplot(aes(x=year, y=die_size_mm_2))+
-  geom_point(size=2)+
+  geom_point(aes(color=type, alpha=0.4))+
   scale_color_manual(values=met.brewer("Egypt"))+
   geom_smooth(method = 'loess', se=FALSE)+
   facet_wrap(~type)+
   labs(
-    title = "Die density (number of transistors per unit area) is increasing for GPUs,driving the increase in transistor count.",
+    title = "Die density (number of transistors per unit area).",
     x = "Year",
     y = "Die size (mm^2)") +
   coord_cartesian(clip="off")+
@@ -76,19 +75,23 @@ p2 <- chips |> ggplot(aes(x=year, y=die_size_mm_2))+
     plot.background = element_rect(color = bg, fill=bg),
     plot.margin = margin(30,30,30,30),
     plot.title.position = "plot",
-    legend.position = "none")
+    legend.position = "none",
+    strip.text = element_text(size = 18))
 
 # PATCHWORK ----------------------------------------------------------
 
 (p1/p2) + 
   plot_annotation(
     title = stringr::str_to_upper("Does Moore's law still hold?"),
-    subtitle = "Moore’s Law predicted that the number of transistors on a chip would double every 18 months.",
+    subtitle = "Moore’s Law predicted that the number of transistors on a chip
+would double every 18 months. Particularly for GPUs, 
+the number of transistors on a device continues to rise rapidly with time.
+Die density is one of the factors driving up transistor count in GPUs.",
     caption = "Muhammad Azhar | #TidyTuesday Week 34 | Data: CHIP dataset ",
     theme = theme(
       plot.title = element_text(size=34, face="bold",
-                                margin=margin(10,0,5,0)),
-      plot.subtitle = element_text(size=18, face="bold",color = "grey40"),
+                                margin=margin(10,0,10,0)),
+      plot.subtitle = element_text(size=16, face="bold",color = "grey40"),
       plot.caption = element_text(hjust=.5, margin=margin(20,0,0,0),
                                   size=12, face="bold"),
       plot.title.position = "plot",
@@ -99,6 +102,6 @@ p2 <- chips |> ggplot(aes(x=year, y=die_size_mm_2))+
 
 showtext_opts(dpi = 320) 
 
-ggsave("week_34.png", height = 10, width = 12, dpi=320)  
+ggsave("week_34.png", height = 10, width = 8, dpi=320)  
 
 showtext_auto(FALSE)

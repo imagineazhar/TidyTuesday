d04c34fb2c30237b26b6736983543339d922d5d3
imagineazhar <azhar116406262@gmail.com>
2022-09-17 18:43:25 +0500

    weel 37

--- 2022/Week_37/week_37.R
+++ 2022/Week_37/week_37.R
@@ -4,6 +4,7 @@ library(showtext)
 library(hrbrthemes)
 library(patchwork)
 library(lubridate)
+library(ggtext)
 
 # Set the Stage ------------------------------------------------
 
@@ -12,7 +13,7 @@ font_add_google(font, font)
 showtext_auto(enable = TRUE)
 theme_set(theme_ipsum(base_family = font))
 bg <- "#F0EBE3"
-text_color <- "black"
+txt_col <- "black"
 
 
 
@@ -33,19 +34,18 @@ df <- bigfoot|>
 # plot 1 ----------------------------------------------------------
 
 p1 <- df|> count(year)|> ggplot(aes(x=year, y=n))+
-  geom_line(size=1) +
-  geom_point(size=1.6)+
+  geom_line(size=1.2, color='#636175') +
+  geom_point(size=1.3, color='#636175')+
   coord_cartesian(clip="off")+
   labs(
-    title = '1990-2004 saw an exponential increase in Bigfoot sightings.',
-    subtitle = " 2004 onwards Bigfoot Sightings have decreased significantly.",
+    title = 'Bigfoot sightings over the years',
+    subtitle = "2004 onwards Bigfoot Sightings have decreased significantly.",
     y = "# of sightings",
-    x = " "
-    )+
+    x = " ")+
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_text(hjust = 0.5),
-    plot.title = element_text(size=20, color=text_color,
+    plot.title = element_text(size=20, color=txt_col,
                               face="bold", margin=margin(0,0,10,0)),
     plot.subtitle =  element_text(size=12, color='grey50',
                               face="bold", margin=margin(0,0,30,0)),
@@ -58,46 +58,41 @@ p1 <- df|> count(year)|> ggplot(aes(x=year, y=n))+
 p1
 
 # plot 2 ----------------------------------------------------------
-p2 <- df |>
-  filter(state!="Alaska", longitude>-130)|>
-  ggplot(aes(longitude, latitude, color="red"))+
-  geom_point()+
-  borders("state")+
-  ggthemes::theme_map()+
-  coord_map()+
+
+top_5 <- df |> count(state, sort = TRUE)|>
+  head(5) |>
+  ggplot(aes(n, y= reorder(state, n)))+
+  geom_col(width = 0.5, fill='#636175')+
+  scale_y_discrete()+
+  labs(title = "Top 5 states with most Sightings")+
   theme(
+    panel.grid = element_blank(),
     axis.title.x = element_blank(),
-    axis.title.y = element_text(hjust = 0.5),
-    plot.title = element_text(size=20, color=text_color,
-                              face="bold", margin=margin(0,0,10,0)),
-    plot.subtitle =  element_text(size=14, color='grey50',
-                                  face="bold", margin=margin(0,0,30,0)),
-    
-    plot.caption = element_text(hjust=.5, margin=margin(20,0,0,0),
-                                size=10, color='grey20', face="plain"),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(size=10, color='grey50'),
+    plot.title = element_text(size=11, color=txt_col,
+                              face="bold", margin=margin(0,0,5,0)),
     plot.background = element_rect(color=bg, fill=bg),
     plot.title.position = "plot",
+    panel.border = element_rect(colour = "black", fill=NA, size=1),
     legend.position = 'hidden')
 
 # Patchwork ----------------------------------------------------------
-(p1 / p2) + plot_annotation(
-  title = 'Finding BigFoot',
-  #  subtitle = 'The amount awarded to each recipient more than doubled.',
-  caption = "Muhammad Azhar | #TidyTuesday Week37 | Data: BFRO",
-  theme = theme(
-    plot.title = element_text(size=34, face="bold",
-                              margin=margin(10,0,10,0)),
-    plot.subtitle = element_text(size=16, face="bold",color = "grey40"),
-    plot.caption = element_text(hjust=.5, margin=margin(20,0,0,0),
-                                size=12, face="bold"),
-    plot.title.position = "plot",
-#    plot.margin = margin(30,10,30,10),
-    plot.background = element_rect(color=bg, fill=bg)))
+
+p1 + inset_element( top_5, 
+                    left = 0.1, bottom = 0.4,
+                    right = 0.6, top = 1,
+                    align_to = 'panel')+
+  plot_annotation(caption = "Muhammad Azhar | #TidyTuesday Week 37 | Data: Data.World/BFRO") &
+  theme(plot.caption = element_text(size=12, color=txt_col,face = "plain", hjust=0.5,
+                                    margin=margin(0,0,0,0), lineheight = 1.4),
+        plot.margin = margin(30,30,30,30),
+        plot.background = element_rect(color=bg, fill=bg))
 
 # Save plot ---------------------------------------------------------- 
 
 showtext_opts(dpi = 320) 
 
-ggsave("week_37.png", height = 10, width = 8, dpi=320)  
+ggsave("week_37.png", height = 8, width = 10, dpi=320)  
 
 showtext_auto(FALSE)

69485fb06a1f09a342d9ddb6198068a1bc280a6c
imagineazhar <2muhammadazhar@gmail.com>
2022-10-03 13:38:58 +0500

    week 39

--- 2022/Week_39/week_39.R
+++ 2022/Week_39/week_39.R
@@ -2,11 +2,10 @@ library(tidyverse)
 library(tidytuesdayR)
 library(showtext)
 library(usmap)
-library(MetBrewer)
 
 # Set the Stage ------------------------------------------------
 
-font <- "Kanit"
+font <- "Roboto"
 font_add_google(family=font, font,db_cache = FALSE)
 showtext_auto(enable = TRUE) 
 theme_set(theme_minimal(base_family = font))
@@ -21,44 +20,38 @@ artists <- tt$artists
 
 df <- artists |>
   filter(type=="Musicians")|>
+  group_by(state)|>
   select(state, race, artists_n, artists_share, location_quotient)|>
-  drop_na()
+  drop_na() |>
+  ungroup()
 
 # plot  ----------------------------------------------------------
 
-p1 <- plot_usmap(data = df, values = "artists_n")+
-  scale_fill_met_c('VanGogh3', type = 'continuous', limits=c(1500,15000))+
-  labs( title="Musicians in the USA",
-  caption = "Muhammad Azhar | #TidyTuesday Week 39 | Data: arts.gov")+
-  theme(
-    panel.grid = element_blank(),
-    axis.title  = element_blank(),
-    axis.text = element_blank(),
-    plot.title = element_text(size=30, color=txt_col, hjust=.5,
-                              face="bold", margin=margin(0,0,30,0)),
-    plot.background = element_rect(color=bg, fill=bg),
-    plot.margin = margin(10,0,0,0),
-    legend.position = 'bottom',
-    legend.background = element_rect(color = bg, fill=bg),
-    legend.justification = "center",
-    legend.title = element_text(size=12),
-    legend.text = element_text(size=10))+
-      guides(fill=guide_legend(title="Number of Musicians",
-                               title.position = "top",
-                               title.hjust = .5,
-                               keyheight = unit(3, units = "mm"),
-                               keywidth=unit(14, units = "mm"),
-                               label.position = "bottom"))
-p1
-
-# Plot 2 -------------------------------------------------------------
-
-df |> group_by(race)|>
-  arrange( desc(artists_n))|>
-  ungroup()|>
-  ggplot(aes(y=as.factor(state), x=artists_n))+
-  geom_col(width = 0.7)+
-  facet_wrap(~race)
+plot_usmap(data = df, values = "artists_n")+
+  scale_fill_continuous(low = "#ecf39e", high = "#007200", limits=c(0,18000),
+                        name="# of Musicians",
+                        guide=guide_legend(
+                          keyheight = unit(3, units = "mm"),
+                          keywidth=unit(12, units = "mm"), 
+                          label.position = "bottom",
+                          title.position = 'top', title.hjust=0.5, nrow=1))+
+  labs( title = "Musicians in United States",
+        subtitle = "California has more than 15000 musicians.",
+        caption = "Muhammad Azhar | #TidyTuesday Week 39 | Data: arts.gov"
+  )+
+  
+  theme(legend.position = 'bottom',
+        legend.background = element_rect(color = bg, fill=bg),
+        legend.justification = "center",
+        plot.title = element_text(size=34, color=txt_col,lineheight=1,
+                                  hjust=0.5,face="bold",
+                                  margin=margin(10,0,10,0)),
+        plot.subtitle = element_text(size=20, color="grey50", face='bold',
+                                     hjust=0.5, margin=margin(0,0,15,0)),
+        plot.caption = element_text(hjust=.5, margin=margin(10,0,0,0),
+                                    size=12, color=txt_col, face="bold"),
+        plot.margin = margin(20,20,20,20),
+        plot.background = element_rect(color=bg, fill=bg))
 
 # Save plot ---------------------------------------------------------- 
 

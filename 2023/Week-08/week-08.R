5f11d3a9c55986443dcf7a8b287f88df6d7d50ff
imagineazhar <2muhammadazhar@gmail.com>
2023-02-22 10:03:26 +0500

    week 8

--- 2023/Week-08/week-08.R
+++ 2023/Week-08/week-08.R
@@ -1,8 +1,6 @@
 library(tidyverse)
-library(ggstream)
 library(tidytuesdayR)
 library(ggtext)
-library(MetBrewer)
 library(showtext)
 
 # ------ Get Data ------ 
@@ -14,13 +12,13 @@ bob_ross <- tt_data$bob_ross
 # ------ Data Wrangling ------ 
 df <- bob_ross|>
   select(painting_index, painting_title,
-         episode, num_colors,
+         season, episode, num_colors,
          color_hex)
-df$season_string <- paste("Season", bob_ross$season, sep = " ")
+# df$season_string <- paste("season", bob_ross$season, sep = " ")
 
 # ------ Typography ------ 
 
-font_add_google("Acme", "title_font")
+font_add_google("Catamaran", "title_font")
 font_add_google("Chivo", "body_font")
 showtext_auto()
 
@@ -44,7 +42,16 @@ caption_text <- "Created by: Muhammad Azhar         Twitter: @imagineazhar"
 df |> ggplot(aes(x=as.factor(episode), y=as.integer(num_colors),))+
   geom_bar(stat="identity", fill='#7286D3', width = 0.8)+
   geom_text(aes(label = num_colors), vjust = 1.5, colour = "black")+
-  facet_wrap(~ season_string, scales = 'free', ncol = 4)+
+  facet_wrap(~ season, labeller = labeller(season=c(
+    '1'='season 1','2'='season 2','3'='season 3', '4'='season 4',
+    '5'='season 5', '6'='season 6', '7'='season 7', '8'='season 8',
+    '9'='season 9', '10'='season 10', '11'='season 11', '12'='season 12',
+    '13'='season 13', '14'='season 14', '15'='season 15', '16'='season 16',
+    '17'='season 17', '18'='season 18', '19'='season 19', '20'='season 20',
+    '21'='season 21','22'='season 22', '23'='season 23', '24'='season 24',
+    '25'='season 25','26'='season 26','27'='season 27','28'='season 28',
+    '29'='season 29', '30'='season 30', '31'='season 31')),
+             scales = 'free', ncol = 4)+
   scale_x_discrete()+
   scale_y_continuous()+
   labs(title = title_text,
